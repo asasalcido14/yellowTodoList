69b9033ccf7aa1bb7491c45d9a3c9933098664e5
asasalcido14 <asasalcido14@email.arizona.edu>
2022-02-06 18:08:47 -0700

    Update InputArea.jsx
    full functionality added with onClick function to add input to array and set the next text to an empty slate. handleChange function

--- yllow-todo/src/components/InputArea.jsx
+++ yllow-todo/src/components/InputArea.jsx
@@ -1,44 +1,26 @@
 import React, { useState } from "react";
-import ToDoItem from "./ToDoItem";
-import InputArea from "./InputArea";
 
-function App() {
-	const [items, setItems] = useState([]);
+function InputArea(props) {
+	const [inputText, setInputText] = useState("");
 
-	function addItem(inputText) {
-		setItems((prevItems) => {
-			return [...prevItems, inputText];
-		});
-	}
-
-	function deleteItem(id) {
-		setItems((prevItems) => {
-			return prevItems.filter((item, index) => {
-				return index !== id;
-			});
-		});
+	function handleChange(event) {
+		const newValue = event.target.value;
+		setInputText(newValue);
 	}
 
 	return (
-		<div className="container">
-			<div className="heading">
-				<h1>To-Do List</h1>
-			</div>
-			<InputArea onAdd={addItem} />
-			<div>
-				<ul>
-					{items.map((todoItem, index) => (
-						<ToDoItem
-							key={index}
-							id={index}
-							text={todoItem}
-							onChecked={deleteItem}
-						/>
-					))}
-				</ul>
-			</div>
+		<div className="form">
+			<input onChange={handleChange} type="text" value={inputText} />
+			<button
+				onClick={() => {
+					props.onAdd(inputText);
+					setInputText("");
+				}}
+			>
+				<span>Add</span>
+			</button>
 		</div>
 	);
 }
 
-export default App;
+export default InputArea;
